0856466105429f8126aa6267cb2f5bea6f5262e6
DAULIN NKONKAM <daulwan@gmail.com>
2024-08-21 02:34:06 +0100

    ajout des composants Header et Hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+  return (
+    <div className="py-[50px] px-5">
+      <div>
+        <h1 className="text-[32px] md:max-w-[840px] md:mx-auto sm:text-[62px] sm:leading-[77.5px] sm:-tracking-[1px]  text-center leading-10 text-gray-900 mx-[34px] ">
+          Trending web & landing page designs
+        </h1>
+        <h2 className="my-6 mx-[70px] md:max-w-[565px] md:mx-auto text-center text-sm sm:text-sm sm:leading-7 leading-7 text-gray-700 ">
+          Explore trending Web Design, Responsive Design, User Experience and
+          User Interfaces from popular design portfolios
+        </h2>
+      </div>
+    </div>
+  );
+};
